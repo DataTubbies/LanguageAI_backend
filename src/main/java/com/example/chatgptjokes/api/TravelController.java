8d6ed3ae7149d226d75d1c30f61aa969febe366f
AdamWarfa <Adamwarfadk@gmail.com>
2024-04-15 17:52:40 +0200

    postreq done

--- src/main/java/com/example/chatgptjokes/api/TravelController.java
+++ src/main/java/com/example/chatgptjokes/api/TravelController.java
@@ -1,6 +1,7 @@
 package com.example.chatgptjokes.api;
 
 import com.example.chatgptjokes.dtos.MyResponse;
+import com.example.chatgptjokes.dtos.TravelDto;
 import com.example.chatgptjokes.service.OpenAiService;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +20,9 @@ public class TravelController {
      */
     final static String SYSTEM_MESSAGE = "You are a helpful assistant that delivers a travel destination, transport, accommodation and activity suggestions." +
             " Keep the answer to a maximum of 1000 words, and within these 4 sections: Destination, Transport, Accommodation, and Activities." +
-            " The user should provide a start location, budget, destination, time of year, and duration. Destination can be optional, in which case you must provide a fittings choice. If an input is lacking anything except the destination, ignore the content of the question and ask the user to provide the necessary inputs.";
+            " The user should provide a start location, budget, destination, time of year, and duration. Destination can be optional, in which case you must provide a fitting choice." +
+            " if starting location is optional, you need to only provide an activity guide" +
+            " If an input is lacking anything except the destination or the starting location, ignore the content of the question and ask the user to provide the necessary inputs.";
 
     /**
      * The controller called from the browser client.
@@ -36,9 +39,36 @@ public class TravelController {
      * @param about contains the input that ChatGPT uses to make a joke about.
      * @return the response from ChatGPT.
      */
-    @GetMapping
-    public MyResponse getTravel(@RequestBody String about) {
+    @PostMapping
+    public MyResponse postTravel(@RequestBody TravelDto about) {
 
-        return service.makeRequest(about, SYSTEM_MESSAGE);
+        String userPrompt;
+
+        if (about.getDestination()==null) {
+            userPrompt = "I want to travel with a budget of " +
+                    about.getBudget() + "danish kroner " +
+                    " from " + about.getStartingLocation() +
+                    " in " + about.getMonth() +
+                    " for " + about.getDuration() + " days, give me a suggestion for a destination.";
+
+        } else if (about.getStartingLocation()==null) {
+            userPrompt = "I want an activity guide with a budget of " +
+                    about.getBudget() + "danish kroner " +
+                    " in " + about.getDestination() +
+                    " in " + about.getMonth() +
+                    " for " + about.getDuration() + " days.";
+        }
+        else {
+            userPrompt = "I want to travel with a budget of " +
+                    about.getBudget() + "danish kroner " +
+                    " to " + about.getDestination() +
+                    " from " + about.getStartingLocation() +
+                    " in " + about.getMonth() +
+                    " for " + about.getDuration() + " days.";
+        }
+
+        return service.makeRequest(userPrompt, SYSTEM_MESSAGE);
     }
 }
+
+
---
+++ src/main/java/com/example/chatgptjokes/dtos/TravelDto.java
@@ -0,0 +1,30 @@
+package com.example.chatgptjokes.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class TravelDto {
+    private int budget;
+    private String destination;
+
+    private String startingLocation;
+
+   private String month;
+
+   private int duration;
+
+
+    public TravelDto(int budget, String destination, String startingLocation, String month, int duration) {
+        this.budget = budget;
+        this.destination = destination;
+        this.startingLocation = startingLocation;
+        this.month = month;
+        this.duration = duration;
+    }
+}
+
+
+
